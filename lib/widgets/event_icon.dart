14459bc30a8ed03f518c776927a521f347f87425
Patrick RAGUIN <patrick@patrick-raguin.fr>
2019-02-21 14:42:29 +0100

    Modification affichage marker event

--- lib/widgets/event_icon.dart
+++ lib/widgets/event_icon.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:sceno_map_flutter/models/event.dart';
 
 class EventIcon extends StatelessWidget {
-
   Event event;
 
-  EventIcon({Key key, this.event}): super(key: key);
+  EventIcon({Key key, this.event}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     var icon = Icon(
       _getIconData(),
       color: _getColor(),
@@ -26,10 +24,22 @@ class EventIcon extends StatelessWidget {
                   contentPadding: EdgeInsets.all(10.0),
                   title: Text(event.title),
                   children: <Widget>[
-                    ListTile(title: Text(event.address), leading: Icon(Icons.location_on, color: Colors.red),),
-                    ListTile(title: Text(event.startDate), leading: Icon(Icons.timer, color: Colors.blue),),
-                    ListTile(title: Text(event.endDate), leading: Icon(Icons.timer_off, color: Colors.grey),),
-                    ListTile(title: Text(event.category), leading: Icon(Icons.category, color: Colors.green),),
+                    ListTile(
+                      title: Text(event.address),
+                      leading: Icon(Icons.location_on, color: Colors.red),
+                    ),
+                    ListTile(
+                      title: Text(event.startDate),
+                      leading: Icon(Icons.timer, color: Colors.blue),
+                    ),
+                    ListTile(
+                      title: Text(event.endDate),
+                      leading: Icon(Icons.timer_off, color: Colors.grey),
+                    ),
+                    ListTile(
+                      title: Text(event.category),
+                      leading: Icon(Icons.bluetooth_audio, color: Colors.blueAccent, size: 20,),
+                    ),
                   ],
                 );
               });
@@ -39,17 +49,17 @@ class EventIcon extends StatelessWidget {
   }
 
   IconData _getIconData() {
-    switch(event.category) {
-      case "Musique" :
+    switch (event.category) {
+      case "Musique":
         return Icons.music_note;
         break;
-      case "Spectacle" :
+      case "Spectacle":
         return Icons.local_activity;
         break;
-      case "CinÃ©ma" :
+      case "CinÃ©ma":
         return Icons.local_movies;
         break;
-      case "LittÃ©rature" :
+      case "LittÃ©rature":
         return Icons.book;
         break;
       default:
@@ -72,11 +82,10 @@ class EventIcon extends StatelessWidget {
       case "Exposition":
         defaultColor = Colors.amber;
         break;
-      case "CinÃ©ma" :
+      case "CinÃ©ma":
         defaultColor.green;
         break;
     }
     return defaultColor;
   }
-
 }
